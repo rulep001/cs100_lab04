45e54bddddedd43bfe04a29810a2a80f728e4045
Ryan Ulep <rulep001@hammer.cs.ucr.edu>
2016-10-24 16:37:42 -0700

    Updated constructors

--- decorator.h
+++ decorator.h
@@ -11,32 +11,34 @@ class Decorator : public Base {
     protected:
 	Base* base;
     public:
-	Decorator(Base* b) : base(b) { }
+	Decorator(Base* b) : base(b) {};
 	double evaluate() {
 		return base->evaluate();
 	}
 };
 
 class CeilDecorator : public Decorator {
-	CeilDirector(Base *b) : Decorator(b) { };
+	public:
+	CeilDecorator(Base* b) : Decorator(b) {};
 	double evaluate() {
 		return ceil(base->evaluate());
 	}
 };
 
 class FloorDecorator : public Decorator {
-        FloorDirector(Base *b) : Decorator(b) { };
+        public:
+	FloorDecorator(Base* b) : Decorator(b) {};
         double evaluate() {
                 return floor(base->evaluate());
         }
 };
 
 class AbsDecorator : public Decorator {
-        AbsDirector(Base *b) : Decorator(b) { };
+        public:
+	AbsDecorator(Base* b) : Decorator(b) {};
         double evaluate() {
                 return abs(base->evaluate());
         }
 };
-
 
 #endif
