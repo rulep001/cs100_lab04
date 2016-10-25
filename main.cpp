7b53d8eab0d4c4e23761195233e59a4609bff0bb
Ryan Ulep <rulep001@hammer.cs.ucr.edu>
2016-10-24 17:45:26 -0700

    Added names to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// Ryan Ulep
+// Michael Pare
+
 #include "Base.h"
 #include "container.h"
 #include "vectorcontainer.h"
@@ -7,10 +10,10 @@
 using namespace std;
 
 int main() {
-	Op *op1 = new Op (-5.3 );
-	Op *op2 = new Op (2.1);
-	Op *op3 = new Op (-4.2);
-	Op *op4 = new Op (6.5);
+	Op *op1 = new Op (-2.2);
+	Op *op2 = new Op (5.5);
+	Op *op3 = new Op (-3.3);
+	Op *op4 = new Op (4.4);
 	Mult *A = new Mult ( op1 ,  op4 );
 	Add *B = new Add ( op3 ,  A );
 	Sub *C = new Sub ( B ,  op2 );
